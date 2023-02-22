ab018f2282971cae76ad6cb0168d94b21d067521
RealGavinPrice <116835834+RealGavinPrice@users.noreply.github.com>
2023-02-22 02:42:16 -0500

    added comment routes js

--- controller/api/commentRoutes.js
+++ controller/api/commentRoutes.js
@@ -2,4 +2,55 @@ const router = require('express').Router();
 
 const { Comment } = require('../../model');
 
-const auth = require('../../util/auth')
+const auth = require('../../util/auth');
+
+
+// GET the comments
+router.get('/', (req, res) => {
+    Comment.findAll()
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// POST a comment
+router.post('/', auth, (req, res) => {
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id
+        })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        })
+    }
+});
+
+
+// DELETE a comment
+router.delete('/:id', auth, (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbCommentData => {
+        if (!dbCommentData) {
+            res.status(404).json({ message: 'This comment does not exist at all!! Comment on it'});
+            return;
+        }
+        res.json(dbCommentData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+module.exports = router;
