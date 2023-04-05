20f7f1fc9b3bca88f6c305e115b6cb6c046d0c51
RealGavinPrice <116835834+RealGavinPrice@users.noreply.github.com>
2023-04-05 00:59:15 -0400

    fixed bugs in hbs and seeds output

--- seeds/commentSeed.js
+++ seeds/commentSeed.js
@@ -4,7 +4,7 @@ const commentData = [
     {
         comment_text: "This is great stuff",
         post_id: 1,
-        user_id: 2,
+        user_id: 1,
     },
     {
         comment_text: "Wow awesomess",
@@ -14,12 +14,12 @@ const commentData = [
     {
         comment_text: "Coolbeans",
         post_id: 1,
-        user_id: 2,
+        user_id: 3,
     },
     {
         comment_text: "Hotbeans",
         post_id: 1,
-        user_id: 2,
+        user_id: 4,
     },
     {
         comment_text: "Beans and rice",
@@ -29,67 +29,67 @@ const commentData = [
     {
         comment_text: "Rice and beans",
         post_id: 2,
-        user_id: 2,
+        user_id: 4,
     },
     {
         comment_text: "Tastes like a good education!",
         post_id: 3,
-        user_id: 2,
+        user_id: 5,
     },
     {
         comment_text: "This is great stuff",
         post_id: 3,
-        user_id: 2,
+        user_id: 1,
     },
     {
         comment_text: "Love this stuff!",
         post_id: 4,
-        user_id: 2,
+        user_id: 3,
     },
     {
         comment_text: "This is great stuff",
         post_id: 4,
-        user_id: 2,
+        user_id: 4,
     },
     {
         comment_text: "Is there paprika in there",
         post_id: 4,
-        user_id: 2,
+        user_id: 3,
     },
     {
         comment_text: "Papython spice",
         post_id: 4,
-        user_id: 2,
+        user_id: 1,
     },
     {
         comment_text: "This is great stuff",
         post_id: 5,
-        user_id: 2,
+        user_id: 5,
     },
     {
         comment_text: "Rice and beans",
         post_id: 5,
-        user_id: 2,
+        user_id: 3,
     },
     {
         comment_text: "This is good stuff",
         post_id: 5,
-        user_id: 2,
+        user_id: 4,
     },
     {
         comment_text: "Never heard about this",
         post_id: 5,
-        user_id: 2,
+        user_id: 1,
     },
     {
         comment_text: "This is great stuff",
         post_id: 6,
-        user_id: 2,
+        user_id: 5,
     },
     {
         comment_text: "This is great stuff 2",
         post_id: 6,
-        user_id: 2,
+        user_id: 3,
     },
 ];
 
