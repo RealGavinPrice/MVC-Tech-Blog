caeaca531b5342109588dabc4adf7da43436fdcc
RealGavinPrice <116835834+RealGavinPrice@users.noreply.github.com>
2023-04-05 00:31:21 -0400

    fixed comment.js bug

--- model/Comment.js
+++ model/Comment.js
@@ -3,7 +3,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 
-class User extends Model {}
+class Comment extends Model {}
 
 Comment.init(
     {
